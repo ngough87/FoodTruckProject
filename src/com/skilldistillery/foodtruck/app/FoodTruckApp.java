b899bb96b73679ff3507409a6a7e49cd3bb996e3
Natasha Gough <natash.gough37@gmail.com>
2022-10-28 17:25:34 -0500

    initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.foodtruck.app;
+
+public class FoodTruckApp {
+//one and only main in the program and only static there will be in this code
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.foodtruck.entities;
+
+//like parking lot class, no main
+public class FoodTruck {
+	private static int nextTruckId;
+	private String name;
+	private int id;
+
+//do not include id as a part of the constructor parameters, the only static method above
+
+
+}
